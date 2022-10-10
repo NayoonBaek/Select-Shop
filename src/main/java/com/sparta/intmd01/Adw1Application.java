ff7028cc082be45f8a6e117e18c3fb8334acb90a
NayoonBaek <vlfdyd57@naver.com>
2022-10-10 17:27:58 +0900

    Select-Shop 1번까지

---
+++ src/main/java/com/sparta/intmd01/Adw1Application.java
@@ -0,0 +1,13 @@
+package com.sparta.intmd01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Adw1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Adw1Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/sparta/intmd01/dto/ProductMypriceRequestDto.java
@@ -0,0 +1,12 @@
+package com.sparta.intmd01.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class ProductMypriceRequestDto {
+    private int myprice;
+}
